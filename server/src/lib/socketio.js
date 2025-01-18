7518811de02b8a6f75dbe5de0fb3d8020b88802f
faukiofficial <faukiofficial@gmail.com>
2025-01-18 15:06:26 +0700

    update socket io cors

--- server/src/lib/socketio.js
+++ server/src/lib/socketio.js
@@ -6,7 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [process.env.CLIENT_URL],
+        origin: process.env.CLIENT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
     }
 });
